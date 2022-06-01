e932a97852320aed091f27fe96df064ec3dff3d2
David Chai <davidchai717@gmail.com>
2022-05-31 21:06:42 -0400

    feat: added contextual menu and removed menubar as a dependency in favor of using Electron directly

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-const { menubar } = require("menubar");
+const { app, Menu, Tray } = require("electron");
 const TickTick = require("ticktick-node-api");
+const ElectronPreferences = require("electron-preferences");
 require("dotenv").config();
 
-const mb = menubar();
+const preferences = new ElectronPreferences({
+  dataStore: "./preferences.json",
+  sections: [
+    {
+      id: "task",
+      label: "Task Preferences",
+      icon: "single-01",
+      form: {
+        groups: [
+          {
+            label: "Sync Schedule",
+            key: "syncSchedule",
+            type: "text",
+            help: "How often should DoThisNow sync with TickTick (in seconds)",
+          },
+          {
+            label: "Task Title Template",
+            key: "titleTemplate",
+            type: "text",
+            help: "Define any prefix or postfix you'd like to add, where {taskName} represents where the task name will be displayed",
+          },
+        ],
+      },
+    },
+  ],
+});
+
 const tt = new TickTick();
 
 let interval;
+let tray;
 
 const getAndSetTask = async () => {
   const incompleteTasks = await tt.getTasks({
@@ -16,22 +44,42 @@ const getAndSetTask = async () => {
     (tasks) => new Date(tasks.startDate) <= new Date()
   );
 
-  mb.tray.setTitle("Do this>>> " + (tasksForToday[0]?.title || "N/A") + " <<<");
+  tray.setTitle("Do this>>> " + (tasksForToday[0]?.title || "N/A") + " <<<");
 };
+app.setName("DoWhatNow");
 
-mb.on("ready", async () => {
-  await tt.login({
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-  });
+app.whenReady().then(async () => {
+  try {
+    tray = new Tray("tick.png");
+    const contextMenu = Menu.buildFromTemplate([
+      { id: 0, label: "Ready to sync...", enabled: false },
+      { type: "separator" },
+      { label: "Sync Now" },
+      {
+        label: "Preferences",
+        click: () => {
+          preferences.show();
+        },
+      },
+      { type: "separator" },
+      { role: "quit" },
+    ]);
+    tray.setContextMenu(contextMenu);
+    await tt.login({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    });
 
-  await getAndSetTask();
+    await getAndSetTask();
 
-  interval = setInterval(() => {
-    getAndSetTask();
-  }, 5000);
+    interval = setInterval(() => {
+      getAndSetTask();
+    }, 5000);
+  } catch (e) {
+    throw e;
+  }
 });
 
-mb.on("window-all-closed", () => {
+app.on("window-all-closed", () => {
   clearInterval(interval);
 });
